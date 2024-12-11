6d9f3588bbea0f0045fba912abc556bc8847ba13
CaoMeiYouRen <996881204@qq.com>
2024-12-12 01:32:02 +0800

    fix: 更新构建工具和依赖；移除缓存步骤以优化工作流   - 将 node 版本要求从 >=12 更新为 >=18   - 移除 rollup 相关依赖和配置   - 添加 tsup 作为新的构建工具   - 更新构建脚本以使用 tsup   - 更新启动脚本以使用 dist/index.js   - 添加 tsup.config.ts 配置文件以定义构建选项   - 移除缓存步骤

---
+++ tsup.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig, Options } from 'tsup'
+
+const tsupOptions: Options = {
+    platform: 'node', // 目标平台
+    entry: ['src/index.ts', 'src/plopfile.ts'],
+    format: ['cjs'],
+    outExtension({ format }) {
+        switch (format) {
+            case 'cjs':
+                return {
+                    js: '.js',
+                    dts: '.d.ts',
+                }
+            case 'esm':
+                return {
+                    js: '.mjs',
+                    dts: '.d.ts',
+                }
+            case 'iife':
+                return {
+                    js: '.global.js',
+                    dts: '.d.ts',
+                }
+            default:
+                break
+        }
+        return {
+            js: '.js',
+            dts: '.d.ts',
+        }
+    },
+    splitting: false, // 代码拆分
+    sourcemap: true,
+    clean: false,
+    dts: false,
+    minify: false, // 缩小输出
+    shims: true, // 注入 cjs 和 esm 填充代码，解决 import.meta.url 和 __dirname 的兼容问题
+    esbuildOptions(options, context) { // 设置编码格式
+        options.charset = 'utf8'
+    },
+    banner: {
+        js: '#!/usr/bin/env node',
+    }
+    // external: [], // 排除的依赖项
+    // noExternal: [/(.*)/], // 将依赖打包到一个文件中
+    // bundle: true,
+}
+
+export default defineConfig([tsupOptions])
