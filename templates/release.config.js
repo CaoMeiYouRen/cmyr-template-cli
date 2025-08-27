0f9f3e15318c28546f639f82f2b7ea12d466387b
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-27 15:54:25 +0800

    refactor: 精简 release 配置，移除多余的插件和逻辑

--- templates/release.config.js
+++ templates/release.config.js
@@ -1,39 +1,6 @@
-import pkg from './package.json' with { type: 'json' }
-const { name } = pkg
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                config: 'conventional-changelog-cmyr-config',
-            },
-        ],
-        [
-            '@semantic-release/release-notes-generator',
-            {
-                config: 'conventional-changelog-cmyr-config',
-            },
-        ],
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-                changelogTitle: `# ${name}`,
-            },
-        ],
-        '@semantic-release/npm',
-        '@semantic-release/github',
-        [
-            '@semantic-release/git',
-            {
-                assets: [
-                    'CHANGELOG.md',
-                    'package.json',
-                ],
-            },
-        ],
-    ],
+    extends: ['semantic-release-cmyr-config'],
 }
