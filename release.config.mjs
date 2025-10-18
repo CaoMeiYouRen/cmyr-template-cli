be0e6464f4cc8b52d279b44c29a6474e74ec5d7c
CaoMeiYouRen <caomeiyouren@qq.com>
2025-10-19 02:02:14 +0800

    chore: 更新依赖项，添加 semantic-release 插件以支持自动化发布

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,39 @@
+import pkg from './package.json' with { type: 'json' }
+const { name } = pkg
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    extends: ['semantic-release-cmyr-config'],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                config: 'conventional-changelog-cmyr-config',
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                config: 'conventional-changelog-cmyr-config',
+            },
+        ],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+                changelogTitle: `# ${name}`,
+            },
+        ],
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: [
+                    'CHANGELOG.md',
+                    'package.json',
+                ],
+            },
+        ],
+    ],
 }
