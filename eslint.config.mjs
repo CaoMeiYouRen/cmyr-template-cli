53cd1fbe0d66a618e4dcef18891981c736ae8f86
CaoMeiYouRen <caomeiyouren@qq.com>
2025-11-15 18:26:46 +0800

    chore: 修复 ESLint 配置，添加最大行数限制规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,8 @@ export default defineConfig([
             'no-console': 0,
             'require-await': 0,
             '@typescript-eslint/no-require-imports': 0,
+            'max-lines': [1, { max: 500 }], // 强制文件的最大行数
+            'max-lines-per-function': [1, { max: 150 }], // 强制函数最大行数
         },
     },
 ])
